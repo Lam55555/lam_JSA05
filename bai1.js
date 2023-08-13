2b01fcccd39b5a7ba4fa2dd8ad3ee770d4630ff7
Lam55555 <nguyenhoanglam121106@gmail.com>
2023-08-13 12:06:01 +0700

    buổi 2

---
+++ bai1.js
@@ -0,0 +1,35 @@
+// let i=0;
+
+// while (x<=10)
+// {
+//     console.log(x++);
+// }
+
+// do {
+// console.log(i++);
+// }while(i<=10);
+
+// for(let i=0; i<=10; i++){console.log(i)}
+
+// let myNumber = [0,1,2];
+// console.log(myNumber[1]);
+
+// for(var i=["Hieu","Ngoc","Tai"]; i<=2;i++); console.log(i);
+
+let myString = ["aa","bb","ab","ba"]
+
+// for(var i=0; i<=4;i++){
+//     console.log(i + " :" + myString[i]);
+// }
+// let i=0;
+// do {
+//     console.log(i + " :" + myString[i]);
+//     i++;
+// }
+// while(i<=3) 
+
+let x=0;
+while(x<=3){
+    console.log(x +":" +myString[x]);
+    x++;
+}
