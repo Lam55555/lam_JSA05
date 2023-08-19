8debd9f09fde56de68d2d4fe1096f9b4223dc7dc
Lam55555 <nguyenhoanglam121106@gmail.com>
2023-08-19 19:09:52 +0700

    baitaptuan2

---
+++ bt/jsbttuan2.js
@@ -0,0 +1,25 @@
+let myFruit=["Grapes",15]
+let i=0;
+while(i<=0)
+{
+    console.log("I have " + myFruit[1]+ " " +myFruit[0]);
+    i++;
+}
+let x=0;
+do {
+    console.log("I have " + myFruit[1]+ " " +myFruit[0]);
+    x++;
+}
+while(x<=0)
+for(let n=0; n<=0; n++) {
+    console.log("I have " + myFruit[1]+ " " +myFruit[0]);
+}
+var Husky = {
+    "name" : "Ngáo", "legs": 4,  "color" : "brown"
+}; 
+// let x=0;
+// let myString = ["aa","bb","ab","ba"]
+// while(x<=3){
+//     console.log(x +":" +myString[3]);
+//     x++;
+// }
