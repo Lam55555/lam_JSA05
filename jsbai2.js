08d26e5258d4410764cfd42b09914f5cbc33507a
Lam55555 <nguyenhoanglam121106@gmail.com>
2023-08-20 11:26:33 +0700

    tuan2

---
+++ jsbai2.js
@@ -0,0 +1,58 @@
+let myArray=[4,5,7,8,9,1]
+console.log(myArray)
+
+// for(let i = 0; i<myArray.length;i++){ console.log("Giá trị của phần tử thứ",i, "là:", myArray[i])}
+
+// myArray[2]=6;
+// console.log(myArray)
+
+myArray.splice(2,4,7,8); console.log(myArray)
+
+// console.log(myArray.length)
+
+// myArray.push(7)
+// console.log(myArray)
+
+// myArray.pop()
+// console.log(myArray)
+
+// myArray.unshift(0) // thêm phần tử ở đầu
+// console.log(myArray)
+
+// myArray.shift() xóa phần tử đầu tiên
+// console.log(myArray)
+
+// let dog={
+//     name:'Alaska', age:4
+// }
+// dog.name='Chihuahua'
+// console.log(dog);
+
+// dog.action='walk'
+// console.log(dog);
+
+// delete(dog.action)
+// console.log(dog)
+
+// let cat = new Object({
+//     name:'Tom',
+//     age:3,
+//     enemy:{
+//         name:'Alaska',
+//         age:5
+//     }
+// });
+// console.log(cat)
+
+// let dog1 = ["Alaska",4]
+// let dog2 = ["Corgi",5]
+// let dogs = [dog1, dog2]
+// console.log(dogs)
+// console.log(dogs[0][1])
+
+// let Giotraicay=[['xoai',2],
+// ['dua hau',3]
+// ]
+// for(let i=0; i<Giotraicay.length;i++){
+//     console.log("Gio trai cay "+ Giotraicay[i][0] + " co " +Giotraicay[i][1] +" trai")
+// }
