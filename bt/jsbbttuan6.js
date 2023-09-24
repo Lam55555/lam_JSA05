e5a20ca375f070a5ecbe63b0e09fc6aab629f061
Lam55555 <nguyenhoanglam121106@gmail.com>
2023-09-24 10:09:54 +0700

    bttuan6

--- bt/jsbbttuan6.js
+++ bt/jsbbttuan6.js
@@ -1,23 +1,24 @@
-function doi5s(){
-    return new Promise(resolve =>
-        {
-            setTimeout(()=>{
-                alert("a")
-            },5000)
-        })
-}
+// function doi5s(){
+//     return new Promise(resolve =>
+//         {
+//             setTimeout(()=>{
+//                 alert("a")
+//             },5000)
+//         })
+// }
 
-async function f(){
-    console.log("Đợi 5s");
-    const namgiay = await doi5s();
-}
-f()
-const user = {
-    name: "John Doe",
-    age: 30,
-    email: "john.doe@example.com",
-    isActive: true
-  };
+// async function f(){
+//     console.log("Đợi 5s");
+//     const namgiay = await doi5s();
+// }
+// f()
+// const user = {
+//     name: "John Doe",
+//     age: 30,
+//     email: "john.doe@example.com",
+//     isActive: true
+//   };
+
   //[
     //     {
     //         "user":[
@@ -42,31 +43,32 @@ const user = {
     //     }
     // ]
 
-const userr = {
-    name: "John Doe",
-    age: 30,
-    address: {
-      street: "123 Main St",
-      city: "New York",
-      country: "USA"
-    }
-  };
-  console.log(userr)
-
-// [
-//     {
-//         "userr":[
-//             {
-//                 name: "John Doe",
-//                 age: 30,
-//                 address: {
-//                   street: "123 Main St",
-//                   city: "New York",
-//                   country: "USA"
-//                          } 
-//             }
-//         ]
+// const userr = {
+//     name: "John Doe",
+//     age: 30,
+//     address: {
+//       street: "123 Main St",
+//       city: "New York",
+//       country: "USA"
 //     }
-    
-// ]
- 
+//   };
+//   console.log(userr)
+
+{ let userr = 
+  {        
+     userr:
+    [
+            {
+                "name": "John Doe",
+                "age": 30,
+                "address": {
+                  "street": "123 Main St",
+                  "city": "New York",
+                  "country": "USA",
+                         },
+            },
+        ]
+  }
+  console.log(userr)
+}
+  
